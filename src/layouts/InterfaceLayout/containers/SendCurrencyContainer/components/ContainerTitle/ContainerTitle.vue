a7fd741bb0ead758595ee4e5bc6ac21651605dbe
David Hong <hsurf22@gmail.com>
2019-07-11 17:30:43 -0700

    Add container title

---
+++ src/layouts/InterfaceLayout/containers/SendCurrencyContainer/components/ContainerTitle/ContainerTitle.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="container-title"></ul>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    titleInfo: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {};
+  },
+
+  computed: {},
+  watch: {},
+  mounted() {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'ContainerTitle.scss';
+</style>
