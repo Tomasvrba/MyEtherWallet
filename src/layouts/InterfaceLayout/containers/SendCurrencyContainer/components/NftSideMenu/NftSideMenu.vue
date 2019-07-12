38304cec758c5c56ffb01b28e3fc36769081864a
David Hong <hsurf22@gmail.com>
2019-07-11 17:04:26 -0700

    Update side menu Add CryptoKitties container

---
+++ src/layouts/InterfaceLayout/containers/SendCurrencyContainer/components/NftSideMenu/NftSideMenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="nft-side-menu">
+    <li
+      v-for="i in data"
+      :key="i.key"
+      :class="i.selected == true ? 'selected' : ''"
+    >
+      {{ i.title }} ({{ i.count }})
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    data: {
+      type: Object,
+      default: function() {
+        return {};
+      }
+    }
+  },
+  data() {
+    return {};
+  },
+
+  computed: {},
+  watch: {},
+  mounted() {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'NftSideMenu.scss';
+</style>
---
+++ src/layouts/InterfaceLayout/containers/SendCurrencyContainer/containers/CryptoKitties/CryptoKitties.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="crypto-kitties"></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    data: {
+      type: Object,
+      default: function() {
+        return {};
+      }
+    }
+  },
+  data() {
+    return {};
+  },
+
+  computed: {},
+  watch: {},
+  mounted() {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'CryptoKitties.scss';
+</style>
